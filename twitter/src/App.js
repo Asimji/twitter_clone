8ae58664935ed22ca8052cf70f3d8f2bb6665c5b
Asim <mohdasim.srk@gmailcom>
2023-11-12 17:57:29 +0530

    sidebar completed

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -5,6 +5,14 @@ import ProtectedRoute from './ProtectedRoute';
 import Login from './pages/login/Login';
 import SignUp from './pages/login/SignUp';
 import PageLoading from './pages/PageLoading';
+import Feed from './pages/feed/Feed';
+import Explore from './pages/explore/Explore';
+import Notification from './pages/notifications/Notification';
+import Messages from './pages/messages/Messages';
+import Bookmarks from './pages/bookmarks/Bookmarks';
+import List from './pages/lists/List';
+import Profile from './pages/profile/Profile';
+import More from './pages/more/More';
 
 function App() {
   return (
@@ -20,6 +28,18 @@ function App() {
         <Route path='/signup' element={<SignUp/>} />
         <Route path='*' element={<h1>This is Wrong Route</h1>} />
         <Route path='/page-loading' element={<PageLoading/>} />
+        <Route path='/home' element={<ProtectedRoute>
+              <Home/>
+          </ProtectedRoute>} >
+            <Route path='feed' element={<Feed/>} />
+            <Route path='explore' element={<Explore/>} />
+            <Route path='notifications' element={<Notification/>} />
+            <Route path='messages' element={<Messages/>} />
+            <Route path='bookmarks' element={<Bookmarks/>} />
+            <Route path='lists' element={<List/>} />
+            <Route path='profile' element={<Profile/>} />
+            <Route path='more' element={<More/>} />
+          </Route>
         
 
       </Routes>
--- twitter/src/pages/Homepage/Home.jsx
+++ twitter/src/pages/Homepage/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Sidebar from '../sidebar/Sidebar'
+import Widget from '../widget/Widget'
+import Feed from '../feed/Feed'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../firebase.init'
+import { signOut } from 'firebase/auth'
+import { Outlet } from 'react-router-dom'
 
 const Home = () => {
+
+  const user=useAuthState(auth)
+
+  const handleLogout=()=>{
+ signOut(auth)
+  }
   return (
-    <div>
-      <h1>This is Homepage</h1>
+    <div className='app'>
+      <Sidebar handleLogout={handleLogout} user={user}/>
+      <Outlet/>
+      <Widget/>
     </div>
   )
 }
